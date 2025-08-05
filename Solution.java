1c5fb444f34487acd3f9af6e89428fe8d2c687be
KalaiselvanR-57 <kalaiselvanr23eele@psnacet.edu.in>
2025-08-05 12:03:52 +0530

    while

---
+++ Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int l=1;
+      while (sc.hasNextLine()){
+        String a=sc.nextLine();
+        System.out.println(l +" " + a);
+        l++;
+       }
+    }}
